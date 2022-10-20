f4064a57b8d67a1217cb6c74c6c5a8bbf35a5c6c
Usuario <Usuario@DESKTOP-C3OFIVD>
2022-10-20 20:36:53 -0300

    Version 0.1: Se testean los getters y los metodos de la version 0.1 de Proyecto

---
+++ test/ar/edu/unq/po2/tpfinal/tests/ProyectoTest.java
@@ -0,0 +1,71 @@
+package ar.edu.unq.po2.tpfinal.tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tpfinal.clases.Muestra;
+import ar.edu.unq.po2.tpfinal.clases.Proyecto;
+import ar.edu.unq.po2.tpfinal.clases.Usuario;
+
+import static org.mockito.Mockito.*;
+
+class ProyectoTest {
+	private Proyecto proyecto;
+	private Usuario usuario;
+	private Muestra muestra;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void testProyectoSabeDevolverSuNombre() {
+		
+		proyecto = new Proyecto("nombre", "descripcion");
+		assertEquals(proyecto.getNombre(), "nombre");
+	}
+	
+	@Test
+	void testProyectoSabeDevolverSuDescripcion() {
+		proyecto = new Proyecto("nombre","descripcion");
+		assertEquals(proyecto.getDescripcion(), "descripcion");
+	}
+	
+	@Test
+	void testProyectoAñadeCategorias() {
+		proyecto = new Proyecto("nombre","descripcion");
+		proyecto.añadirCategoria("Una categoria");
+		
+		assertTrue(proyecto.getListaDeCategorias().contains("Una categoria"));
+	}
+	
+	@Test
+	void testProyectoAñadeUsuario() {
+		usuario = mock(Usuario.class);
+		proyecto = new Proyecto("nombre","descripcion");
+		proyecto.suscribirUsuario(usuario);
+		
+		assertTrue(proyecto.getListaDeUsuarios().contains(usuario));
+	}
+	
+	@Test
+	void testProyectoAñadeMuestra() {
+		muestra = mock(Muestra.class);
+		proyecto = new Proyecto("nombre","descripcion");
+		proyecto.añadirMuestra(muestra);
+		
+		assertTrue(proyecto.getListaDeMuestras().contains(muestra));
+	}
+	
+	@Test
+	void testProyectoDesuscribeUsuario() {
+		usuario = mock(Usuario.class);
+		proyecto = new Proyecto("nombre","descripcion");
+		proyecto.desuscribirUsuario(usuario);
+		
+		assertFalse(proyecto.getListaDeUsuarios().contains(usuario));
+	}
+	
+}
