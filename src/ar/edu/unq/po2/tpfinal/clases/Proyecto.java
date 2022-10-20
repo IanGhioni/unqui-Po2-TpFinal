c66de9bd82e0a3edede599818f5ea8ea833da3ee
Usuario <Usuario@DESKTOP-C3OFIVD>
2022-10-20 20:32:54 -0300

    Version 0.1 Proyecto sabe su nombre, su descripcion, sus listas de usuarios, muestras y categorias y sabe añadir categorias, muestras y suscribir y desuscribir usuarios de su lista de usuarios.

---
+++ src/ar/edu/unq/po2/tpfinal/clases/Proyecto.java
@@ -0,0 +1,66 @@
+package ar.edu.unq.po2.tpfinal.clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Proyecto {
+
+	private String nombre;
+	private String descripcion;
+	private List<String> listaDeCategorias;
+	private List<Usuario> listaDeUsuariosSuscriptos;
+	private List<Muestra> listaDeMuestras;
+
+	public Proyecto(String nombre, String descripcion) {
+		this.setNombre(nombre);
+		this.setDescripcion(descripcion);
+		this.listaDeCategorias = new ArrayList<String>();
+		this.listaDeUsuariosSuscriptos = new ArrayList<Usuario>();
+		this.listaDeMuestras = new ArrayList<Muestra>();
+	}
+
+	private void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void añadirCategoria(String categoria) {
+		this.listaDeCategorias.add(categoria);
+	}
+
+	public List<String> getListaDeCategorias() {
+		return this.listaDeCategorias;
+	}
+
+	public List<Usuario> getListaDeUsuarios() {
+		return this.listaDeUsuariosSuscriptos;
+	}
+
+	public void suscribirUsuario(Usuario usuario) {
+		this.listaDeUsuariosSuscriptos.add(usuario);
+	}
+
+	public void añadirMuestra(Muestra muestra) {
+		this.listaDeMuestras.add(muestra);
+	}
+
+	public List<Muestra> getListaDeMuestras() {
+		return this.listaDeMuestras;
+	}
+
+	public void desuscribirUsuario(Usuario usuario) {
+		this.listaDeUsuariosSuscriptos.remove(usuario);
+	}
+}
