931c211c466357ae1542920ac445c5f06e7acee7
sergio ruffinelli <sergio@ruffinelli.com.ar>
2022-10-25 18:10:19 -0300

    clases Estructura de filtrador de proyectos y clases Cfalse y CTrue

--- src/ar/edu/unq/po2/tpfinal/CName/CName.java
+++ src/ar/edu/unq/po2/tpfinal/CName/CName.java
@@ -1,4 +1,7 @@
 package ar.edu.unq.po2.tpfinal.CName;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.List;
 import ar.edu.unq.po2.tpfinal.filtrador.*;
 import ar.edu.unq.po2.tpfinal.evaluable.*;
 import ar.edu.unq.po2.tpfinal.condicion.*;
@@ -7,7 +10,14 @@ import java.util.ArrayList; // import the ArrayList class
 
 
 public class  CName extends Condicion{
+	private Pattern pattern;
+	public CName(String regexString) {
+		this.pattern = Pattern.compile(regexString);
+	}
 	public ArrayList<Evaluable> evaluar(ArrayList<Evaluable> lista) {
-		return lista;
+		return   (ArrayList<Evaluable>) lista
+			      .stream()
+			      .filter(e -> pattern.matcher(e.getName()).matches())
+			      .collect(Collectors.toList());
 	}
 }
