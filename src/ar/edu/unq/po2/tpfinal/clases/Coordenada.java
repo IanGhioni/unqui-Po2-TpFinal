8fbb07ee9e4691992e01740fe8c6b087a0845403
Usuario <Usuario@DESKTOP-C3OFIVD>
2022-10-25 16:54:01 -0300

    Version 0.1: Se implementaron los getters de la coordenada.

---
+++ src/ar/edu/unq/po2/tpfinal/clases/Coordenada.java
@@ -0,0 +1,20 @@
+package ar.edu.unq.po2.tpfinal.clases;
+
+public class Coordenada {
+	private float x;
+	private float y;
+	
+	public Coordenada(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+}
