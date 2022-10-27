17a04fa35e0a35344dd07dd1e17de1788d205eea
sergio ruffinelli <sergio@ruffinelli.com.ar>
2022-10-27 16:31:59 -0300

    Filtro terminado

---
+++ src/ar/edu/unq/po2/tpfinal/BAnd/BAnd.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.po2.tpfinal.BAnd;
+import ar.edu.unq.po2.tpfinal.filtrador.*;
+import ar.edu.unq.po2.tpfinal.evaluable.*;
+import ar.edu.unq.po2.tpfinal.condicion.*;
+import java.util.List; // import the ArrayList class
+import java.util.ArrayList; // import the ArrayList class
+
+
+
+public class  BAnd extends   Filtrador{
+	Filtrador filtrador1;
+	Filtrador filtrador2;
+	public BAnd(Filtrador filtrador1, Filtrador filtrador2) {
+		this.filtrador1 = filtrador1;
+		this.filtrador2 = filtrador2;
+	}
+	public ArrayList<Evaluable> evaluar(ArrayList<Evaluable> lista) {
+		
+		ArrayList<Evaluable> resultado1 = this.filtrador1.evaluar(lista);
+		ArrayList<Evaluable> resultado2 = this.filtrador2.evaluar(lista);
+		ArrayList<Evaluable> resultado = resultado1;
+		resultado=resultado1;
+		resultado.retainAll(resultado2);
+		return  resultado;
+	}
+}
