65a76ea468d13d348fd08d7a56abdec30716eb4e
unknown <sergio@ruffinelli.com.ar>
2022-11-05 17:31:09 -0300

    a

--- src/ar/edu/unq/po2/tpfinal/usuario/PerfilDeUsuario.java
+++ src/ar/edu/unq/po2/tpfinal/usuario/PerfilDeUsuario.java
@@ -19,6 +19,7 @@ public class PerfilDeUsuario {
 		return recompensa;
 	}
 
+
 	public double calcularCoincidenciaConDesafio(Desafio d) {
 		return(
 			Math.abs(d.getDificultad() - this.dificultad)
@@ -28,5 +29,6 @@ public class PerfilDeUsuario {
 			Math.abs(d.getRecompensa() - this.recompensa)
 		);
 	}
+
 
 }
